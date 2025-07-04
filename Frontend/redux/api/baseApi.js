a4fe3516b4debd83ae49c9478f2de61d2a5c0632
devoleper-samreen <samreenmalik52292@gmail.com>
2025-07-04 20:32:38 +0530

    Refactor base API query and improve ResetPassword component structure

--- Frontend/redux/api/baseApi.js
+++ Frontend/redux/api/baseApi.js
@@ -1,38 +1,37 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/",
-    credentials: "include",
+  baseUrl: "https://videotube-fu4j.onrender.com/api/",
+  credentials: "include",
 });
 
 // Custom Base Query jo Refresh Token Handle karega
 const customBaseQuery = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
 
-    if (result?.error?.status == 401) {
+  if (result?.error?.status == 401) {
+    const refreshResult = await baseQuery(
+      {
+        url: "user/refresh-access-token",
+        method: "POST",
+      },
+      api,
+      extraOptions
+    );
 
-        const refreshResult = await baseQuery({
-            url: "user/refresh-access-token",
-            method: "POST",
-        }, api, extraOptions);
-
-
-        // Agar 401
-        if (refreshResult?.error?.status == 401) {
-            console.log('refresh token bhi exipre ho gaya');
-        }
-
-        // Agar naya access token mila
-        if (refreshResult?.data?.data?.accessToken) {
-
-            // **Original Request Retry karein with New Token**
-            result = await baseQuery(args, api, extraOptions);
-        }
-
+    // Agar 401
+    if (refreshResult?.error?.status == 401) {
+      console.log("refresh token bhi exipre ho gaya");
     }
 
-    return result;
-};
+    // Agar naya access token mila
+    if (refreshResult?.data?.data?.accessToken) {
+      // **Original Request Retry karein with New Token**
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
 
+  return result;
+};
 
 export default customBaseQuery;
--- Frontend/src/components/ResetPassword.jsx
+++ Frontend/src/components/ResetPassword.jsx
@@ -1,83 +1,91 @@
 import React, { useState } from "react";
 import { Container, TextField, Button, Typography, Box } from "@mui/material";
-import Loader from "./Loader";
-import { useResetPasswordMutation } from "../../redux/api/auth"
+// import Loader from "./Loader";
+import { useResetPasswordMutation } from "../../redux/api/auth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 const ResetPassword = () => {
-    const { id, token } = useParams();
-    const navigate = useNavigate();
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [resetPassword, { isLoading, isError }] = useResetPasswordMutation();
+  const { id, token } = useParams();
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [resetPassword, { isLoading, isError }] = useResetPasswordMutation();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        try {
-            const res = await resetPassword({ id, token, newPassword: password, confiremPassword: confirmPassword }).unwrap();
-            console.log("response : ", res);
+    try {
+      const res = await resetPassword({
+        id,
+        token,
+        newPassword: password,
+        confiremPassword: confirmPassword,
+      }).unwrap();
+      console.log("response : ", res);
 
-            toast.success(res?.message);
-            navigate("/login");
+      toast.success(res?.message);
+      navigate("/login");
+    } catch (error) {
+      console.error("Forgot Password Failed:", error);
+      toast.error(error.data?.message || "Password reset failed");
+    }
+  };
 
-        } catch (error) {
-            console.error("Forgot Password Failed:", error);
-            toast.error(error.data?.message || "Password reset failed");
-
-        }
-
-    };
-
-    return (
-        <Container maxWidth="sm" sx={{ mt: 10, borderTop: 4, borderColor: "primary.main", boxShadow: 3, borderRadius: 2 }}>
-            <Box sx={{ textAlign: "center", mt: 3, p: 3, borderRadius: 2 }}>
-                <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
-                    Reset Password
-                </Typography>
-                <Typography variant="body2" color="textSecondary" gutterBottom>
-                    Enter new password to reset your password.
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        fullWidth
-                        label="New Password"
-                        variant="outlined"
-                        onChange={(event) =>
-                            setPassword(event.target.value)}
-                        type="password"
-                        value={password}
-                        sx={{ mt: 6 }}
-                        helperText={isError ? "Enter a valid  password" : ""}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Confirm Password"
-                        variant="outlined"
-                        onChange={(event) =>
-                            setConfirmPassword(event.target.value)}
-                        type="password"
-                        value={confirmPassword}
-                        sx={{ mt: 3 }}
-                        helperText={isError ? "Enter a valid  password" : ""}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        disabled={!password || isError}
-                        sx={{ mt: 4 }}
-                    >
-                        {isLoading ? <Loader /> : "Submit"}
-
-                    </Button>
-                </form>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        mt: 10,
+        borderTop: 4,
+        borderColor: "primary.main",
+        boxShadow: 3,
+        borderRadius: 2,
+      }}
+    >
+      <Box sx={{ textAlign: "center", mt: 3, p: 3, borderRadius: 2 }}>
+        <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
+          Reset Password
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          Enter new password to reset your password.
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="New Password"
+            variant="outlined"
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            value={password}
+            sx={{ mt: 6 }}
+            helperText={isError ? "Enter a valid  password" : ""}
+          />
+          <TextField
+            fullWidth
+            label="Confirm Password"
+            variant="outlined"
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            type="password"
+            value={confirmPassword}
+            sx={{ mt: 3 }}
+            helperText={isError ? "Enter a valid  password" : ""}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={!password || isError}
+            sx={{ mt: 4 }}
+          >
+            {isLoading ? "Loading..." : "Submit"}
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
 };
 
 export default ResetPassword;
